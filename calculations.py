cbb02132c37979dc80af2bfcd3c564544cacdc4f
SDGaming317 <134094466+SDGaming317@users.noreply.github.com>
2024-11-27 16:36:52 -0500

    Updated calculations.py

--- calculations.py
+++ calculations.py
@@ -1,7 +1,9 @@
-'''from scipy.io import wavfile
+from scipy.io import wavfile
 import scipy.io
+import matplotlib.pyplot as plt
+import numpy as np
 
-wav_fname = '16bitstereoFX.wav'
+wav_fname = '16bit4chan.wav'
 samplerate, data = wavfile.read(wav_fname)
 print(f"number of channels = {data.shape[len(data.shape) - 1]}")
 print(f"sample rate = {samplerate}Hz")
@@ -9,31 +11,8 @@ length = data.shape[0] / samplerate
 print(f"length = {length}s")
 
 
-import matplotlib.pyplot as plt
-import numpy as np
 time = np.linspace(0., length, data.shape[0])
-numpy.linspace(start, stop, num=50, endpoint=True, retstep=False, dtype=None,
-axis=0)[source]
-
-import matplotlib.pyplot as plt
-import numpy as np
-time = np.linspace(0., length, data.shape[0])
-
-
-import matplotlib.pyplot as plt
-import numpy as np
-time = np.linspace(0., length, data.shape[0])
-plt.plot(time, data[:, 0], label="Left channel")
-plt.plot(time, data[:, 1], label="Right channel")
-plt.legend()
-plt.xlabel("Time [s]")
-plt.ylabel("Amplitude")
-plt.show()'''
-
-'''
-NEEDS:
-- 
-'''
+print(time)
 
 wavelength = 0
 velocity = 0
