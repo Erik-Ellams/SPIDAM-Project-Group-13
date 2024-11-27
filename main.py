8ec56bb5e34c2dfc889696598fb0deeb02038bf6
Mabric Corcuera <mabriccorcuera@gmail.com>
2024-11-27 15:41:10 -0500

    Created the plot

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import tkinter as tk
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
 
 # Create the main window
 window = tk.Tk()
@@ -13,3 +21,4 @@ label.pack()
 
 # Start the event loop
 window.mainloop()
+
