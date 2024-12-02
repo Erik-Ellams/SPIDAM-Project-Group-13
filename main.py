fe09ae244d460a7736ac3712de5992b5c23991ed
Mabric Corcuera <mabriccorcuera@gmail.com>
2024-12-01 21:11:36 -0500

    Buttons not really fixed (final version)

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class audioLoader:
     def _create_buttons(self):
         window = self.window
         button_frame = tk.Frame(self.window)
-        button_frame.grid(row=4, column=0, columnspan=5, pady=10)
+        button_frame.grid(row=4, column=0, columnspan=6, pady=20)
 
         load_button = tk.Button(window, text="Load Audio", command=self.load_audio, width=50, height=5)
         load_button.grid(row=0, column=2, pady=10)
@@ -58,23 +58,23 @@ class audioLoader:
 
         # Create a "Load RT60 Low" button
         RT60LOW_button = tk.Button(window, text="Load RT60 Low Plot", command=lambda: self.display_rt60("low"), width=20)
-        RT60LOW_button.grid(row=4, column=1, pady=5)
+        RT60LOW_button.grid(row=4, column=1, pady=5, padx=10)
 
         # Create a "Load RT60 Mid" button
         RT60MID_button = tk.Button(window, text="Load RT60 MID Plot", command=lambda: self.display_rt60("medium"), width=20)
-        RT60MID_button.grid(row=4, column=2, pady=5)
+        RT60MID_button.grid(row=4, column=2, pady=5, padx=10)
 
         # Create a "Load RT60 HIGH" button
         RT60HIGH_button = tk.Button(window, text="RT60 HIGH Plot", command=lambda: self.display_rt60("high"), width=20)
-        RT60HIGH_button.grid(row=4, column=3, pady=5)
+        RT60HIGH_button.grid(row=4, column=3, pady=5, padx=10)
 
         # Create a combined RT60 plot
         Combine_plots_button = tk.Button(window, text="Combined RT60 Plot", command=lambda: self.display_combined(), width=20)
-        Combine_plots_button.grid(row=4, column=4)
+        Combine_plots_button.grid(row=4, column=4, pady=5, padx=10)
 
         # Reduce the reverb
         reduce_reverb_button = tk.Button(self.window, text="Reduce Reverb to 0.5s", command=self.reduce_reverb, width=20)
-        reduce_reverb_button.grid(row=4, column=5, pady=5)
+        reduce_reverb_button.grid(row=4, column=5, pady=5, padx=10)
 
     def _create_result_frames(self):
         self.duration_label = self._create_result_frame("Audio Duration", 5, 0)
